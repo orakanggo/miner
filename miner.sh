b68a26e3eb74dabc77e9f974d201ff5208cb14ba
orakanggo <orakanggo@gmail.com>
2021-11-28 12:13:37 +0700

    Create miner.sh

---
+++ miner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+echo "Get XMRig v6.16.0"
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.0/xmrig-6.16.0-macos-x64.tar.gz
+
+echo "Set Huge Pages"
+
+# https://xmrig.com/docs/miner/hugepages#onegb-huge-pages
+
+sysctl -w vm.nr_hugepages=$(nproc)
+
+for i in $(find /sys/devices/system/node/node* -maxdepth 0 -type d);
+do
+    echo 3 > "$i/hugepages/hugepages-1048576kB/nr_hugepages";
+done
+
+echo "1GB pages successfully enabled"
